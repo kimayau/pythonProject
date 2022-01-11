f843514f7d374f6c8ca969f0a3c84d4d5dda45d8
kimayau <51640674+kimayau@users.noreply.github.com>
2022-01-11 10:13:23 +0530

    Add files via upload

---
+++ Functionality_of_Data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+class Data:
+
+    def make_db_connection(self):
+        return Active_connection
+
+    def __init__(self,filepath):
+        self.filepath = filepath
+
+    def get_filepath(self):
+        return self.filepath
+
+    def readfile(filepath):
+         return pd.read_csv("updated_stock_tickers.csv")
+
+    def load_into_dataframe("updated_stock_tickers.csv"):
+        return df
+
+
+
+
+
+
+
+
+
+
